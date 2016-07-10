840c1aefd752d5bf41ebe8abc41797007ffcbd66
Jenia <gr.yevhen@gmail.com>
2016-07-11 00:03:07 +0300

    refactor game.html

--- src/servlet/Constant.java
+++ src/servlet/Constant.java
@@ -1,10 +1,13 @@
 package servlet;
 
 public interface Constant {
+	// Game servlet
 	String RANGE_TYPE = "rangeType";
 	String RANDOM = "random";
 	String MIN_VALUE = "minValue";
 	String MAX_VALUE = "maxValue";
 	String SECRET_NUMBER = "secretNumber";
 	String REDIRECT_LINK = "./game.html";
+	
+	// Check result servlet
 }
