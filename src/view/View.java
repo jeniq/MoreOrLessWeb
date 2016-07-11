effa8a530f0330a7b69496363cc4cb946c9fb73c
Jenia <gr.yevhen@gmail.com>
2016-07-12 00:20:57 +0300

    Clean view

--- src/view/View.java
+++ src/view/View.java
@@ -10,14 +10,13 @@ package view;
  *
  */
 public class View {
-	public static final String WRONG_ATTEMPT = "<div align = \"center\">Your attemp is wrong, try again";
-	public static final String HIGHER_NUMBER = "<br>Your number is higher than secret one. </div>";
-	public static final String LOWER_NUMBER = "<br>Your number is lower than secret one. </div>";
-	public static final String CONGRATULATION = "<div align = \"center\"><b>Congratulation!</b>"
-			+ "<br>You guessed the secret number!</div>";
+	public static final String WRONG_ATTEMPT = "Your attemp is wrong, try again";
+	public static final String HIGHER_NUMBER = "Your number is higher than secret one.";
+	public static final String LOWER_NUMBER = "Your number is lower than secret one.";
+	public static final String CONGRATULATION = "Congratulation!"
+			+ "You guessed the secret number!";
 	public static final String NULL_SECRET_NUMBER = "Oops, something went wrong, try new game.";
-	public static final String ATTEMPS_TITLE = "<br>Attemps";
-	public static final String NEW_LINE = "<br>";
+	public static final String ATTEMPS_TITLE = "Attemps";
 	public static final String IN_RANGE = " in range (";
 	public static final String SEMICOLON = ";";
 	public static final String CLOSING_BRACKET = ")";
